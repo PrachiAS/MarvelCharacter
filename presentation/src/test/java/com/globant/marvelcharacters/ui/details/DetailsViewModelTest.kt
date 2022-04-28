ef761408497a4099701d4b2c622f9992e4a45460
Prachi Jadhav <prachi.jadhav@globant.com>
2022-04-28 15:03:52 +0530

    Added Unit test cases for presentation layer

--- presentation/src/test/java/com/globant/marvelcharacters/ui/details/DetailsViewModelTest.kt
+++ presentation/src/test/java/com/globant/marvelcharacters/ui/details/DetailsViewModelTest.kt
@@ -1,25 +1,45 @@
 package com.globant.marvelcharacters.ui.details
 
+import com.globant.marvelcharacters.domain.usecase.MarvelCharacterDetailsUseCase
+import com.globant.marvelcharacters.mockMarvelCharacterDetailsResponse
 import com.globant.marvelcharacters.ui.BaseViewModelTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.any
+import org.mockito.kotlin.whenever
 
 @ExperimentalCoroutinesApi
 class DetailsViewModelTest : BaseViewModelTest() {
 
     @Mock
-    private lateinit var mapper: CharacterDetailMapper
+    private lateinit var getMarvelCharacterDetailsUseCase: MarvelCharacterDetailsUseCase
 
     private lateinit var viewModel: DetailsViewModel
 
     @Before
     fun before() {
         MockitoAnnotations.openMocks(this)
-        viewModel = DetailsViewModel(
-            mapper
-        )
+        viewModel = DetailsViewModel(getMarvelCharacterDetailsUseCase)
     }
 
+    @Test
+    fun `getMarvelCharacterDetails should get characterDetails from network`() {
+        testScope.runBlockingTest {
+            whenever(getMarvelCharacterDetailsUseCase.executeUseCase(any())
+            ).thenReturn(
+                mockMarvelCharacterDetailsResponse
+            )
+            Assert.assertEquals(viewModel.characterDetailsModelLiveData.value,
+                mockMarvelCharacterDetailsResponse)
+
+            Assert.assertEquals(viewModel.showSuccess.value, true)
+            Assert.assertEquals(viewModel.showError.value, false)
+            Assert.assertEquals(viewModel.showLoading.value, false)
+        }
+    }
 }
--- presentation/src/test/java/com/globant/marvelcharacters/ui/home/HomeViewModelTest.kt
+++ presentation/src/test/java/com/globant/marvelcharacters/ui/home/HomeViewModelTest.kt
@@ -5,6 +5,7 @@ import com.globant.marvelcharacters.mockGetMarvelCharacterListResponse
 import com.globant.marvelcharacters.ui.BaseViewModelTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
@@ -15,30 +16,28 @@ import org.mockito.kotlin.whenever
 @ExperimentalCoroutinesApi
 class HomeViewModelTest : BaseViewModelTest() {
 
-   /* @Mock
-    private lateinit var searchCharacterUseCase: MarvelCharacterListUseCase
+    @Mock
+    private lateinit var getMarvelCharacterListUseCase: MarvelCharacterListUseCase
+
     private lateinit var viewModel: HomeViewModel
 
     @Before
     fun before() {
         MockitoAnnotations.openMocks(this)
-        viewModel =
-            HomeViewModel(
-                searchCharacterUseCase
-            )
-    }*/
-/*
-    @Test
-    fun `onItemClickListener should post value to characterDetails`() {
-        testScope.runBlockingTest {
-            whenever(
-                searchCharacterUseCase.executeUseCase(
-                    any()
-                )
-            ).thenReturn(
-                //mockGetMarvelCharacterListResponse
-            )
+        viewModel = HomeViewModel(getMarvelCharacterListUseCase)
+    }
 
+    @Test
+    fun `onItemClickListener should post id to characterDetails`() {
+        testScope.runBlockingTest {
+            whenever(getMarvelCharacterListUseCase.executeUseCase(any())
+            ).thenReturn(
+                mockGetMarvelCharacterListResponse
+            )
+            assertEquals(viewModel.characterNameList.value, mockGetMarvelCharacterListResponse)
+            assertEquals(viewModel.showSuccess.value, true)
+            assertEquals(viewModel.showError.value, false)
+            assertEquals(viewModel.showLoading.value, false)
         }
-    }*/
+    }
 }
